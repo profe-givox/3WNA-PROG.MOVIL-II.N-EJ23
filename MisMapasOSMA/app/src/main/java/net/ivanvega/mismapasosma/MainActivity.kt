011458dc3cc3d245dfd5f761e6499e17315dece4
Gustavo Ivan Vega Olvera <gi.vega@itsur.edu.mx>
2023-04-21 09:41:55 -0600

    Se configuro firebase Cloud Messinging

--- MisMapasOSMA/app/src/main/java/net/ivanvega/mismapasosma/MainActivity.kt
+++ MisMapasOSMA/app/src/main/java/net/ivanvega/mismapasosma/MainActivity.kt
@@ -1,34 +1,212 @@
 package net.ivanvega.mismapasosma
 
 
+import android.Manifest
+import android.annotation.SuppressLint
+import android.content.BroadcastReceiver
 import android.content.Context
+import android.content.IntentFilter
+import android.content.IntentSender
+import android.location.Location
+import android.location.LocationManager
 import android.os.Bundle
+import android.os.Looper
 import android.preference.PreferenceManager
 import android.view.View
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
+import com.google.android.gms.common.api.ResolvableApiException
+import com.google.android.gms.location.*
+import com.google.android.gms.tasks.Task
+import net.ivanvega.mismapasosma.location.LocationProviderChangedReceiver
+import net.ivanvega.mismapasosma.location.MyEventLocationSettingsChange
+import org.osmdroid.api.IMapController
 import org.osmdroid.config.Configuration
 import org.osmdroid.tileprovider.tilesource.TileSourceFactory
 import org.osmdroid.util.GeoPoint
 import org.osmdroid.views.MapView
 import org.osmdroid.views.overlay.*
+import timber.log.Timber
 
 
 class MainActivity : AppCompatActivity() {
+    lateinit  var mapController: IMapController
     private var firstMarker: Marker? = null
     private lateinit var marker: Marker
-    var map: MapView? = null
+    lateinit var map: MapView
 
+    private lateinit var activityResultLauncher: ActivityResultLauncher<Array<String>>
+    private lateinit var fusedLocationClient: FusedLocationProviderClient //https://developer.android.com/training/location/retrieve-current
+    private var lastLoction: Location? = null
+    private var locationCallback: LocationCallback
+    private var locationRequest: LocationRequest
+    private var requestingLocationUpdates = false
+
+    companion object {
+        val REQUEST_CHECK_SETTINGS = 20202
+    }
+
+    init{
+        locationRequest = LocationRequest.create()
+            .apply { //https://stackoverflow.com/questions/66489605/is-constructor-locationrequest-deprecated-in-google-maps-v2
+                interval = 1000 //can be much higher
+                fastestInterval = 500
+                smallestDisplacement = 10f //10m
+                priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+                maxWaitTime = 1000
+            }
+
+        locationCallback = object : LocationCallback() {
+            override fun onLocationResult(locationResult: LocationResult) {
+                super.onLocationResult(locationResult)
+                locationResult ?: return
+                for (location in locationResult.locations) {
+                    // Update UI with location data
+                    updateLocation(location) //MY function
+                }
+            }
+            /*override fun onLocationResult(locationResult: LocationResult?) {
+                locationResult ?: return
+                for (location in locationResult.locations) {
+                    // Update UI with location data
+                    updateLocation(location) //MY function
+                }
+            }*/
+        }
+        this.activityResultLauncher = registerForActivityResult(
+            ActivityResultContracts.RequestMultiplePermissions()
+        ) { result ->
+            var allAreGranted = true
+            for (b in result.values) {
+                allAreGranted = allAreGranted && b
+            }
+
+            Timber.d("Permissions granted $allAreGranted")
+            if (allAreGranted) {
+                initCheckLocationSettings()
+                //initMap() if settings are ok
+            }
+        }
+    }
+
+    fun initMap() {
+        initLoaction()
+        if (!requestingLocationUpdates) {
+            requestingLocationUpdates = true
+            startLocationUpdates()
+        }
+        mapController.setZoom(18.5)
+        mapController.setCenter(startPoint);
+        map.invalidate()
+    }
+
+    fun initLoaction() { //call in create
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+        readLastKnownLocation()
+    }
+
+    //https://developer.android.com/training/location/retrieve-current
+    @SuppressLint("MissingPermission") //permission are checked before
+    fun readLastKnownLocation() {
+        fusedLocationClient.lastLocation
+            .addOnSuccessListener { location: Location? ->
+                location?.let { updateLocation(it) }
+            }
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun startLocationUpdates() { //onResume
+        fusedLocationClient.requestLocationUpdates(
+            locationRequest,
+            locationCallback,
+            Looper.getMainLooper()
+        )
+    }
+
+    fun updateLocation(newLocation: Location) {
+        lastLoction = newLocation
+        //GUI, MAP TODO
+
+        this.title = "Lat: ${newLocation.latitude.toString()}, " +
+                "Lon: ${newLocation.longitude.toString()}"
+
+        //var currentPoint: GeoPoint = GeoPoint(newLocation.latitude, newLocation.longitude);
+        startPoint.longitude = newLocation.longitude
+        startPoint.latitude = newLocation.latitude
+        mapController?.setCenter(startPoint)
+        getPositionMarker().position = startPoint
+        map?.invalidate()
+
+    }
+
+    private fun getPositionMarker(): Marker { //Singelton
+        if (marker == null) {
+            marker = Marker(map)
+            marker!!.title = "Here I am"
+            marker!!.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_BOTTOM)
+            marker!!.icon = ContextCompat.getDrawable(this, R.drawable.ic_position);
+            map.overlays.add(marker)
+        }
+        return marker!!
+    }
+
+    fun initCheckLocationSettings() {
+        val builder = LocationSettingsRequest.Builder()
+            .addLocationRequest(locationRequest)
+        val client: SettingsClient = LocationServices.getSettingsClient(this)
+        val task: Task<LocationSettingsResponse> = client.checkLocationSettings(builder.build())
+        task.addOnSuccessListener { locationSettingsResponse ->
+            Timber.d("Settings Location IS OK")
+            MyEventLocationSettingsChange.globalState = true //default
+            initMap()
+            // All location settings are satisfied. The client can initialize
+            // location requests here.
+            // ...
+        }
+
+        task.addOnFailureListener { exception ->
+            if (exception is ResolvableApiException) {
+                // Location settings are not satisfied, but this can be fixed
+                // by showing the user a dialog.
+                Timber.d("Settings Location addOnFailureListener call settings")
+                try {
+                    // Show the dialog by calling startResolutionForResult(),
+                    // and check the result in onActivityResult().
+                    exception.startResolutionForResult(
+                        this@MainActivity,
+                        REQUEST_CHECK_SETTINGS
+                    )
+                } catch (sendEx: IntentSender.SendIntentException) {
+                    // Ignore the error.
+                    Timber.d("Settings Location sendEx??")
+                }
+            }
+        }
+
+    }
+
+    var startPoint: GeoPoint = GeoPoint(46.55951, 15.63970);
     //your items
     var items = ArrayList<OverlayItem>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        //handle permissions first, before map is created. not depicted here
+        val br: BroadcastReceiver = LocationProviderChangedReceiver()
+        val filter = IntentFilter(LocationManager.PROVIDERS_CHANGED_ACTION)
+        registerReceiver(br, filter)
 
-        //load/initialize the osmdroid configuration, this can be done
         //handle permissions first, before map is created. not depicted here
+        val appPerms = arrayOf(
+            Manifest.permission.ACCESS_FINE_LOCATION,
+            Manifest.permission.ACCESS_NETWORK_STATE,
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE,
+            Manifest.permission.INTERNET
+        )
+        activityResultLauncher.launch(appPerms)
 
         //load/initialize the osmdroid configuration, this can be done
         val ctx: Context = applicationContext
@@ -52,10 +230,10 @@ class MainActivity : AppCompatActivity() {
         map = findViewById<View>(R.id.map) as MapView
         map!!.setTileSource(TileSourceFactory.MAPNIK)
 
-        val mapController = map!!.controller
+        mapController = map!!.controller
         mapController.setZoom(19)
-        val startPoint = GeoPoint(20.140153689100682, -101.15067778465794)
-        mapController.setCenter(startPoint)
+        //val startPoint = GeoPoint(20.140153689100682, -101.15067778465794)
+        //mapController.setCenter(startPoint)
 
         items.add(
             OverlayItem(
@@ -69,9 +247,9 @@ class MainActivity : AppCompatActivity() {
         firstMarker?.position = startPoint
         firstMarker?.setAnchor(Marker.ANCHOR_BOTTOM, Marker.ANCHOR_CENTER)
         firstMarker?.title = "Bello ITSUR"
-        map?.overlays?.add(firstMarker)
+        //map?.overlays?.add(firstMarker)
 
-        map?.invalidate()
+
 
         //add your points here
         val line = Polyline();   //see note below!
@@ -84,6 +262,7 @@ class MainActivity : AppCompatActivity() {
                 )
         )
         map?.overlays?.add(line)
+        map?.invalidate()
     }
 
     override fun onResume() {
@@ -97,11 +276,19 @@ class MainActivity : AppCompatActivity() {
 
     override fun onPause() {
         super.onPause()
+        if (requestingLocationUpdates) {
+            requestingLocationUpdates = false
+            stopLocationUpdates()
+        }
         //this will refresh the osmdroid configuration on resuming.
         //if you make changes to the configuration, use
         //SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
         //Configuration.getInstance().save(this, prefs);
         map!!.onPause() //needed for compass, my location overlays, v6.0.0 and up
+    }
+
+    private fun stopLocationUpdates() { //onPause
+        fusedLocationClient.removeLocationUpdates(locationCallback)
     }
 
 }
